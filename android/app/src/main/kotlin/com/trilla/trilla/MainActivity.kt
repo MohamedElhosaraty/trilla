61545d66126dbafcb0425809863884050fe8e9bd
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-01-24 22:42:58 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/trilla/trilla/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trilla.trilla
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:trilla/screen/login.dart';
+import 'package:trilla/screen/test.dart';
+import 'package:trilla/screen/welcome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: Locale("ar" , ""),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Favourite(),
+    );
+  }
+}
+
+
